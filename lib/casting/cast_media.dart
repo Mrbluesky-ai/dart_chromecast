84e64d8157524f911b4d411c76e87409035fc502
lukas <broem.broem@outlook.com>
2023-01-26 23:08:04 +0100

    subtitles added

--- lib/casting/cast_media.dart
+++ lib/casting/cast_media.dart
@@ -26,42 +26,80 @@ class CastMedia {
   }
 
   Map toChromeCastMap() {
-    return {
-      'type': 'LOAD',
-      'autoPlay': autoPlay,
-      'currentTime': position,
-      'playbackRate': playbackRate,
-      'activeTracks': [],
-      'media': {
-        'contentId': contentId,
-        'contentType': contentType,
-        'streamType': 'BUFFERED',
-        'textTrackStyle': {
-          'edgeType': 'NONE', // can be: "NONE", "OUTLINE", "DROP_SHADOW", "RAISED", "DEPRESSED"
-          'fontScale': 1.0, // transforms into "font-size: " + (fontScale*100) +"%"
-          'fontStyle': 'NORMAL', // can be: "NORMAL", "BOLD", "BOLD_ITALIC", "ITALIC",
-          'fontFamily': 'Droid Sans',
-          'fontGenericFamily': 'SANS_SERIF', // can be: "SANS_SERIF", "MONOSPACED_SANS_SERIF", "SERIF", "MONOSPACED_SERIF", "CASUAL", "CURSIVE", "SMALL_CAPITALS",
-          'windowColor': '#00000', // see http://dev.w3.org/csswg/css-color/#hex-notation
-          'windowRoundedCornerRadius': 10, // radius in px
-          'windowType': 'NONE' // can be: "NONE", "NORMAL", "ROUNDED_CORNERS"
-        },
-        'metadata': {
-          'metadataType': 0,
-          'images': images?.map((image) => {'url': image}).toList(),
-          'title': title,
-          'subtitle': subtitle,
-        },
-        subtitlesUrl == ""? 'tracks': {
-          'trackId': 10, // This is an unique ID, used to reference the track
-          'type': 'TEXT', // Default Media Receiver currently only supports TEXT
-          'trackContentId': subtitlesUrl, // the URL of the VTT (enabled CORS and the correct ContentType are required)
-          'trackContentType': 'text/vtt', // Currently only VTT is supported
-          'name': 'Dutch', // a Name for humans
-          'language': 'nl-NL', // the language
-          'subtype': 'SUBTITLES' // should be SUBTITLES
-        } : null,
-      }
-    };
+    if (subtitlesUrl == ""){
+      return {
+        'type': 'LOAD',
+        'autoPlay': autoPlay,
+        'currentTime': position,
+        'playbackRate': playbackRate,
+        'activeTracks': [],
+        'media': {
+          'contentId': contentId,
+          'contentType': contentType,
+          'streamType': 'BUFFERED',
+          'textTrackStyle': {
+            'edgeType': 'NONE',
+            'fontScale': 1.0,
+            'fontStyle': 'NORMAL',
+            'fontFamily': 'Droid Sans',
+            'fontGenericFamily': 'SANS_SERIF',
+            'windowColor': '#00000',
+            'windowRoundedCornerRadius': 10,
+            'windowType': 'NONE',
+          },
+          'metadata': {
+            'metadataType': 0,
+            'images': images?.map((image) => {'url': image}).toList(),
+            'title': title,
+            'subtitle': subtitle,
+          },
+        }
+      };
+    } else {
+      return {
+        'type': 'LOAD',
+        'autoPlay': autoPlay,
+        'currentTime': position,
+        'playbackRate': playbackRate,
+        'activeTracks': [],
+        'media': {
+          'contentId': contentId,
+          'contentType': contentType,
+          'streamType': 'BUFFERED',
+          'textTrackStyle': {
+            'edgeType': 'NONE',
+            'fontScale': 1.0,
+            'fontStyle': 'NORMAL',
+            'fontFamily': 'Droid Sans',
+            'fontGenericFamily': 'SANS_SERIF',
+            'windowColor': '#00000',
+            'windowRoundedCornerRadius': 10,
+            'windowType': 'NONE',
+          },
+          'metadata': {
+            'metadataType': 0,
+            'images': images?.map((image) => {'url': image}).toList(),
+            'title': title,
+            'subtitle': subtitle,
+          },
+          'tracks': {
+            'trackId': 10,
+            // This is an unique ID, used to reference the track
+            'type': 'TEXT',
+            // Default Media Receiver currently only supports TEXT
+            'trackContentId': subtitlesUrl,
+            // the URL of the VTT (enabled CORS and the correct ContentType are required)
+            'trackContentType': 'text/vtt',
+            // Currently only VTT is supported
+            'name': 'Dutch',
+            // a Name for humans
+            'language': 'nl-NL',
+            // the language
+            'subtype': 'SUBTITLES',
+            // should be SUBTITLES
+          }
+        }
+      };
+    }
   }
 }
